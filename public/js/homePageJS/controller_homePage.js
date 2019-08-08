88842d3e58ff6b80397dc854c6634bc62442b63c
Orlando <otrejosm@ucenfotec.ac.cr>
2019-08-08 12:13:06 -0600

    QUITAR HOLA

--- public/js/homePageJS/controller_homePage.js
+++ public/js/homePageJS/controller_homePage.js
@@ -3,6 +3,4 @@ if(sessionStorage.getItem("nombre") == null){
     window.location.href = ("login.html");
 }
 
-//HOLA
-
 document.getElementById("usrName").innerHTML = sessionStorage.getItem("nombre");S
